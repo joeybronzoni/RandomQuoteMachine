38f9b96d47acf92558c006a1afd318af7799eb26
josephskrzysowski <josephskrzysowski@gmail.com>
2017-10-05 19:13:20 -0400

    ajax calls work, now its on to creating the api for the user

--- javascript/app.js
+++ javascript/app.js
@@ -10,28 +10,60 @@
 $( document ).ready(function() {
   console.log("ready");
 
-$.ajax({
-  url: 'https://favqs.com/api/qotd',
-  method: "GET",
-  dataType:"json"
-    }).done(function(response) {
-      console.log(response.quote.id);
-      $('#display').html("response")
+// $.ajax({
+//   url: 'https://favqs.com/api/qotd',
+//   method: "GET",
+//   dataType:"json"
+//     }).done(function(response) {
+//       console.log(response.quote);
+//       let results = response.quote;
+//       let me = $.parseHTML(results)
+//       $(".display").html("<div id='newDiv'>"+ "<h1>"+results.author+"</h1>"+"<h2>"+results.tags+"</h2>"+"<p>"+results.body+"</p>"+   "</div>");
+//       //$("#olDiv").html("<div id='olDiv'>"+results.tags+"</div>");
+//        //$("#oDiv").html("<div id='oDiv'>"+results.body+"</div>");
+//       console.log(results.author);
+//       console.log(results.tags);
+//       console.log(results.body);
+//       console.log(me);
+//     });
+  $('#addQoute').on('click', function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: 'https://favqs.com/api/qotd',
+      method: "GET",
+      dataType:"json"
+    }).done((res) => {
+      console.log(res.quote);
+      let results = res.quote;
+      let me = $.parseHTML(results)
+      $(".display").html("<div id='newDiv'>"+ "<h1>"+results.author+"</h1>"+"<h2>"+results.tags+"</h2>"+"<p>"+results.body+"</p>"+   "</div>");
     });
 
+    
+  });
 
   
-$('#addQouteFunny').on('click', function(e) {
-  e.preventDefault();
-$.ajax({
-  url: 'https://favqs.com/api/qotd',
-  method: "GET",
-  dataType:"json"
-    }).done(function(response) {
-      console.log(response.qotd_date[qoute]);
-      $('#display').html("response")
-    });
-});
+// $('#addQouteFunny').on('click', function(e) {
+//   e.preventDefault();
+
+// $.ajax({
+//   url: 'https://favqs.com/api/qotd',
+//   method: "GET",
+//   dataType:"json"
+//     }).done(function(response) {
+//       console.log(response.quote);
+//       let results = response.quote;
+//       let me = $.parseHTML(results)
+//       $(".display").html("<div id='newDiv'>"+ "<h1>"+results.author+"</h1>"+"<h2>"+results.tags+"</h2>"+"<p>"+results.body+"</p>"+   "</div>");
+//       //$("#olDiv").html("<div id='olDiv'>"+results.tags+"</div>");
+//        //$("#oDiv").html("<div id='oDiv'>"+results.body+"</div>");
+//       console.log(results.author);
+//       console.log(results.tags);
+//       console.log(results.body);
+//       console.log(me);
+//     });
+
+
   
 
 
